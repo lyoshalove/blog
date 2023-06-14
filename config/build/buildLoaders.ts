d1f8b67a5952a38e92744c6f1e427f393ea1ccf5
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-06-14 22:56:11 +0400

    feat: add babel, extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -44,5 +44,22 @@ export const buildLoaders = ({
     ],
   };
 
-  return [typescriptLoader, styleLoader, svgLoader, fileLoader];
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            { locales: ["ru", "en"], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  };
+
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, styleLoader];
 };
