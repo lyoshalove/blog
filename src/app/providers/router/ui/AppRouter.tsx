05936281363dac9bc2ae34ded43baf01f8a16758
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-06-17 15:32:38 +0400

    feat: add not found page and loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
 import { FC, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 export const AppRouter: FC = () => (
-  <Suspense fallback={<h1>Загрузка...</h1>}>
+  <Suspense fallback={<PageLoader />}>
     <div className="page">
       <Routes>
         {Object.values(routeConfig).map(({ path, element }) => (
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,24 +1,31 @@
-import { AboutPage } from 'pages/about';
-import { MainPage } from 'pages/main';
+import { AboutPage } from 'pages/About';
+import { MainPage } from 'pages/Main';
+import { NotFoundPage } from 'pages/NotFound';
 import { RouteProps } from 'react-router-dom';
 
 export enum AppRoute {
-  MAIN = 'main',
-  ABOUT = 'about',
+  MAIN = 'Main',
+  ABOUT = 'About',
+  NOT_FOUND = 'NotFound'
 }
 
 export const RoutePath: Record<AppRoute, string> = {
   [AppRoute.MAIN]: '/',
   [AppRoute.ABOUT]: '/about',
+  [AppRoute.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoute, RouteProps> = {
   [AppRoute.MAIN]: {
-    path: RoutePath.main,
+    path: RoutePath.Main,
     element: <MainPage />,
   },
   [AppRoute.ABOUT]: {
-    path: RoutePath.about,
+    path: RoutePath.About,
     element: <AboutPage />,
+  },
+  [AppRoute.NOT_FOUND]: {
+    path: RoutePath.NotFound,
+    element: <NotFoundPage />,
   },
 };
