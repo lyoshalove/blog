a73bdb1ce5c6c86e3bf5f388ca4cbfde3a10d87f
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-07-25 22:23:54 +0400

    refactor: add Suspense to every page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -4,13 +4,15 @@ import { routeConfig } from 'shared/config/routeConfig';
 import { PageLoader } from 'widgets/PageLoader';
 
 export const AppRouter: FC = () => (
-  <Suspense fallback={<PageLoader />}>
-    <div className="page">
-      <Routes>
-        {Object.values(routeConfig).map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Routes>
-    </div>
-  </Suspense>
+  <div className="page">
+    <Routes>
+      {Object.values(routeConfig).map(({ path, element }) => (
+        <Route
+          key={path}
+          path={path}
+          element={<Suspense fallback={<PageLoader />}>{element}</Suspense>}
+        />
+      ))}
+    </Routes>
+  </div>
 );
