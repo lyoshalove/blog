c90cfbd15ec7b8114548e35215319443913dce39
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-08-17 20:25:21 +0400

    refactor: change default opened modal value in storybook

--- src/shared/ui/Modal/modal.stories.tsx
+++ src/shared/ui/Modal/modal.stories.tsx
@@ -17,7 +17,7 @@ export const Primary: Story = {
   args: {
     children:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, maiores! Harum ipsa hic amet voluptatum ea voluptate sit soluta enim neque, deserunt tenetur. Aperiam neque maiores quos ut harum excepturi?",
-    isOpened: true,
+    isOpened: false,
   },
 };
 
@@ -25,7 +25,7 @@ export const PrimaryDark: Story = {
   args: {
     children:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, maiores! Harum ipsa hic amet voluptatum ea voluptate sit soluta enim neque, deserunt tenetur. Aperiam neque maiores quos ut harum excepturi?",
-    isOpened: true,
+    isOpened: false,
   },
   decorators: [ThemeDecorator(Theme.DARK)],
 };
